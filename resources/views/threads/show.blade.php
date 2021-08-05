fa70f7787e012ee57024e6c29950d827c899a78e
Salman Patnee <salmanpatni92@gmail.com>
2021-08-06 00:10:24 +0500

    A thread has comments

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,15 +2,34 @@
 
 @section('content')
     <div class="container">
-
-    <div class="card mb-3">
-        <div class="card-body">
-            <h5 class="card-title">
-                {{$thread->title}}
-            </h5>
-        <p class="card-text">{{$thread->body}}</p>
+        <div class="row">
+            <div class="col-sm-12">
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        {{$thread->title}}
+                    </h5>
+                <p class="card-text">{{$thread->body}}</p>
+                </div>
+            </div>
         </div>
-    </div>
+        </div>
+        
+            @foreach ($thread->replies as $reply)
+                <div class="row">
+                    <div class="col-sm-8">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <a href="#">{{$reply->owner->name}}</a> said {{$reply->created_at->diffForHumans()}}
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">{{$reply->body}}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        
        
     </div>
 @endsection
